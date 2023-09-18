d5e3155d28012dd19999f84b412302678989ef1a
xund-novak <83659015+xund-novak@users.noreply.github.com>
2023-09-18 22:50:28 +0200

    Add files via upload

---
+++ Sources/PrinterPackage/Printer.swift
@@ -0,0 +1,16 @@
+//
+//  Printer.swift
+//  PrinterPackage
+//
+//  Created by Dániel Novák on 2023. 09. 18..
+//
+
+import Foundation
+
+public struct Printer {
+    public init() { }
+    
+    public func printText(_ text: String) {
+        print(text)
+    }
+}
